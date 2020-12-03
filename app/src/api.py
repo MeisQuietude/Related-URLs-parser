53946a4a761287879fd5f56fa6018b1f587e5e4e
Stepan Savelyev <stepan.savelyev>
2020-12-03 18:19:47 +0500

    [IMP] Simple API class

---
+++ app/src/api.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+import requests
+from validator_collection import validators
+
+
+class AbstractAPI(ABC):
+    @staticmethod
+    @abstractmethod
+    def get(url: str):
+        pass
+
+
+class API(AbstractAPI):
+    @staticmethod
+    def get(url_raw: str) -> requests.Response:
+        """
+        Request URL (GET)
+
+        Raises:
+             validators.EmptyValueError,
+             validators.CannotCoerceError,
+             validators.InvalidURLError,
+             requests.ConnectionError
+        """
+        url = validators.url(url_raw)  # Raises: InvalidURLError
+        response = requests.get(url)  # Raises: ConnectionError
+        return response
