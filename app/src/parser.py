85f0ae19220d17a507a893af0391d9fb576cf160
Stepan Savelyev <stepan.savelyev>
2020-12-06 00:44:18 +0500

    [IMP] Parse related URLs

--- app/src/parser.py
+++ app/src/parser.py
@@ -1,8 +1,8 @@
 from abc import ABC, abstractmethod
 from functools import cached_property
-from typing import Iterator
+from typing import Set, Iterable
 
-from bs4 import BeautifulSoup, ResultSet
+from bs4 import BeautifulSoup
 
 
 class AbstractParser(ABC):
@@ -13,8 +13,20 @@ class AbstractParser(ABC):
 
     @property
     @abstractmethod
-    def anchor_nodes(self) -> Iterator:
+    def anchor_nodes(self) -> Iterable:
         pass
+
+    @abstractmethod
+    def get_related_anchors_href(self) -> Iterable[str]:
+        pass
+
+    @staticmethod
+    def _is_href_url_related(url: str) -> bool:
+        return not (
+            url is None or
+            url.startswith('#') or
+            url == '/'
+        )
 
 
 class ParserBS(AbstractParser):
@@ -23,16 +35,30 @@ class ParserBS(AbstractParser):
     """
 
     def __init__(self, html_raw: str, parser_bs_type: str = "html.parser"):
-        self.html_raw = html_raw
         self.html_parsed = BeautifulSoup(html_raw, parser_bs_type)
+
+    @property
+    def html_raw(self):
+        return self.html_parsed.__str__()
 
     @cached_property
     def title(self):
         return self.html_parsed.find("title").text
 
     @cached_property
-    def anchor_nodes(self) -> ResultSet:
+    def anchor_nodes(self) -> Iterable:
         return self.html_parsed.find_all("a", attrs={"href": True})
+
+    def get_related_anchors_href(self) -> Iterable[str]:
+        collection: Set[str] = set()
+
+        for node in self.anchor_nodes:
+            href: str = node.attrs.get("href")
+            if not ParserBS._is_href_url_related(href):
+                continue
+            collection.add(href)
+
+        return collection
 
     def __repr__(self):
         return self.html_parsed.__repr__()
