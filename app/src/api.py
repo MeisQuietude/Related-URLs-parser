cab973a29e21ad509764df46da64193b0a424f64
Stepan Savelyev <stepan.savelyev>
2020-12-08 19:30:23 +0500

    [IMP] Async API requests

--- app/src/api.py
+++ app/src/api.py
@@ -1,34 +1,45 @@
+import asyncio
 from typing import Iterable
 
+import aiohttp
 import requests
-from aiohttp import ClientSession
 
 from app.src import Logger
-from app.src.validator import Validator
 
 
 class API(object):
+    @staticmethod
+    def get(url: str):
+        return requests.get(url)
 
     @staticmethod
-    def get(url: str) -> requests.Response:
-        """
-        Request URL (GET)
-        """
-        try:
-            response = requests.get(url)
+    async def fetch_urls_async(
+        urls: Iterable[str], bounded_semaphore: asyncio.BoundedSemaphore
+    ) -> Iterable[aiohttp.ClientResponse]:
+        async with aiohttp.ClientSession(
+            connector=aiohttp.TCPConnector(verify_ssl=False)
+        ) as session:
+            tasks = [
+                API.__async__fetch_url(session, url, bounded_semaphore)
+                for url in urls
+            ]
+            responses: Iterable[aiohttp.ClientResponse] = await asyncio.gather(
+                *tasks)
+            for response in responses:
+                Logger.info(f"{response.url.__str__()} - {response.status}")
+                # response.html = await response.text()
+
+            return responses
+
+    @staticmethod
+    async def __async__fetch_url(
+        session: aiohttp.ClientSession,
+        url: str,
+        bounded_semaphore: asyncio.BoundedSemaphore
+    ) -> aiohttp.ClientResponse:
+        async with \
+            bounded_semaphore, session.get(url, allow_redirects=True) \
+                as response:
+            # I should not probably await anything here
+            response.html = await response.text(encoding="utf-8")
             return response
-        except Exception as error:
-            Logger.warning(
-                f"Request to `{url} failed due to error: {error.__str__()}`"
-            )
-
-
-class API(object):
-
-    async def get_all(self, urls: Iterable[str]):
-        async with ClientSession() as session:
-            try:
-                Validator.is_valid_email()
-            except Exception as error:
-                session.request(method='GET', url=url)
-                pass
