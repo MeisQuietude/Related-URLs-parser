e38a6ac452db385095c99b7418d5e5ceeae9116b
Stepan Savelyev <stepan.savelyev>
2020-12-08 19:29:39 +0500

    [IMP] Module name and line number in a logger message

--- app/src/api.py
+++ app/src/api.py
@@ -1,28 +1,34 @@
-from abc import ABC, abstractmethod
+from typing import Iterable
 
 import requests
-from validator_collection import validators
+from aiohttp import ClientSession
+
+from app.src import Logger
+from app.src.validator import Validator
 
 
-class AbstractAPI(ABC):
+class API(object):
+
     @staticmethod
-    @abstractmethod
-    def get(url: str):
-        pass
-
-
-class API(AbstractAPI):
-    @staticmethod
-    def get(url_raw: str) -> requests.Response:
+    def get(url: str) -> requests.Response:
         """
         Request URL (GET)
-
-        Raises:
-             validators.EmptyValueError,
-             validators.CannotCoerceError,
-             validators.InvalidURLError,
-             requests.ConnectionError
         """
-        url = validators.url(url_raw)
-        response = requests.get(url)
-        return response
+        try:
+            response = requests.get(url)
+            return response
+        except Exception as error:
+            Logger.warning(
+                f"Request to `{url} failed due to error: {error.__str__()}`"
+            )
+
+
+class API(object):
+
+    async def get_all(self, urls: Iterable[str]):
+        async with ClientSession() as session:
+            try:
+                Validator.is_valid_email()
+            except Exception as error:
+                session.request(method='GET', url=url)
+                pass
