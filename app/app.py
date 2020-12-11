0dc2e25e3c5b9e7a56333acaa373619b939219e1
Stepan Savelyev <stepan.savelyev>
2020-12-11 12:16:51 +0500

    [IMP] Abstract type annotation

--- app/app.py
+++ app/app.py
@@ -1,18 +1,19 @@
 from typing import Type, Union
 
+from . import AppProvider
 from .get import CmdGetProvider
 from .load import CLILoadProvider
 from .src.cli import args
 
 
-def execute_command() -> Type[Union[str, CmdGetProvider, CLILoadProvider]]:
+def execute_command() -> Union[Type[AppProvider], None]:
     return {
         "get": CmdGetProvider,
         "load": CLILoadProvider,
-    }.get(args.method.lower(), str)
+    }.get(args.method.lower(), None)
 
 
 def main():
     command = execute_command()()
-    if type(command) == str:
+    if command is None:
         return print(f"Unexpected method: `{args.method}`")
